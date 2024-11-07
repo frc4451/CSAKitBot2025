af21bb54463ff56d136d0e3bb3a2366508c8063a
Drivers Station <4451driverstation@gmail.com>
2024-11-07 18:41:38 -0500

    Used Custom Controller in Robot Container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.ControllerConstants;
+import frc.robot.controllers.CommandCustomXboxController;
 import frc.robot.subsystems.drive.DriveSubsystem;
 
 /**
@@ -21,7 +22,7 @@ import frc.robot.subsystems.drive.DriveSubsystem;
  */
 public class RobotContainer {
     // Replace with CommandPS4Controller or CommandJoystick if needed
-    private final CommandXboxController driverController = new CommandXboxController(
+    private final CommandCustomXboxController driverController = new CommandCustomXboxController(
             ControllerConstants.kDriverControllerPort);
 
     private final DriveSubsystem driveSubsystem = new DriveSubsystem();
