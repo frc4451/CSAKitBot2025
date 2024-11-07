1488991603cdeb46df3e23bbd2d13b5d55c55605
Drivers Station <4451driverstation@gmail.com>
2024-11-07 18:40:58 -0500

    Added custom controller class (courtesy of Ember)

---
+++ src/main/java/frc/robot/controllers/ControllerConstants.java
@@ -0,0 +1,6 @@
+package frc.robot.controllers;
+
+public class ControllerConstants {
+    public static final double kJoystickDeadband = 0.1;
+    public static final double kTriggerPressedThreshold = 0.1;
+}
