60a768b000f298dedb4d3e34e600d48c27a52477
Victoria Z <victoriazamora007@gmail.com>
2025-03-21 20:28:15 -0400

    Arm & Intake/Outtake commands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,13 +5,17 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Constants.ControllerConstants;
 import frc.robot.controllers.CommandCustomXboxController;
 import frc.robot.subsystems.drive.DriveREVSubsystem;
 import frc.robot.subsystems.drive.DriveSPXSubsystem;
+import frc.robot.subsystems.arm.ArmSubsystem;
+import frc.robot.subsystems.IntakeWheels.IntakeSubsystem;
 
 /**
  * This class is where the bulk of the robot should be declared. Since
@@ -27,11 +31,33 @@ public class RobotContainer {
     // private final CommandCustomXboxController driverController = new CommandCustomXboxController(ControllerConstants.kDriverControllerPort);
     // private final CommandJoystick driverController = new CommandJoystick(ControllerConstants.kDriverControllerPort);
 
-    private final CommandCustomXboxController driverController = new CommandCustomXboxController(ControllerConstants.kDriverControllerPort);
+    private final XboxController driverController = new XboxController(ControllerConstants.kDriverControllerPort);
 
     // private final DriveREVSubsystem driveSubsystem = new DriveREVSubsystem();
     private final DriveSPXSubsystem driveSubsystem = new DriveSPXSubsystem();
     // private final RollerSubsystem rollerSubsystem = new RollerSubsystem();
+
+    private final ArmSubsystem armSubsystem = new ArmSubsystem();
+
+    private final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
+
+    private final JoystickButton moveOutButton = new JoystickButton(driverController, XboxController.Button.kLeftBumper.value);
+    private final JoystickButton moveInButton = new JoystickButton(driverController, XboxController.Button.kRightBumper.value);
+
+    //for the intake wheels
+    private final JoystickButton spinIntakeButton = new JoystickButton(driverController, XboxController.Button.kA.value);
+    private final JoystickButton spinOuttakeButton = new JoystickButton(driverController, XboxController.Button.kY.value);
+
+    //Moves the arm outwards while left bumper is held; stops the motor once left bumper is released
+    private final Command moveOut = Commands.startEnd(() -> {armSubsystem.moveUp();}, () -> {armSubsystem.stop();}, armSubsystem);
+
+    //Moves the arm inwards while right bumper is held; stops the motor once right bumper is released
+    private final Command moveIn = Commands.startEnd(() -> {armSubsystem.moveDown();}, () -> {armSubsystem.stop();}, armSubsystem);
+    
+    private final Command spinIntake = Commands.startEnd(()->{intakeSubsystem.spinIntake();}, () -> {intakeSubsystem.stop();}, intakeSubsystem);
+
+    private final Command spinOuttake = Commands.startEnd(()->{intakeSubsystem.spinOuttake();}, () -> {intakeSubsystem.stop();}, intakeSubsystem);
+
 
     public RobotContainer() {
         configureBindings();
@@ -46,6 +72,12 @@ public class RobotContainer {
 
         // driverController.rightTrigger().whileTrue(rollerSubsystem.runVoltsCommand(3));
         // driverController.leftTrigger().whileTrue(rollerSubsystem.runVoltsCommand(-3));
+
+        moveOutButton.whileTrue(moveOut);
+        moveInButton.whileTrue(moveIn);
+
+        spinIntakeButton.whileTrue(spinIntake);
+        spinOuttakeButton.whileTrue(spinOuttake);
     }
 
     public Command getAutonomousCommand() {
